6855cbaba1c0182c78cb9f5b60af44f08b0eda40
Vinay Siwach <56073955+vinaysiwachvs@users.noreply.github.com>
2023-07-04 16:09:46 +0530

    adding possibility

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
     <>
   <p><a href='#home'>Home</a></p>
   <p><a href='#wgpt3'>What is GPT3? </a></p>
-  <p><a href='#possiblity'>Open AI</a></p>
+  <p><a href='#possibility'>Open AI</a></p>
   <p><a href='#features'>Case Studies</a></p>
   <p><a href='#blog'>Library</a></p>
   </>
